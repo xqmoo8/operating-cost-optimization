2a10a9e5d95f28a695ccb806a773c21f5f8885df
xqmoo8 <xuqimin_01@126.com>
2017-07-11 10:03:03 +0800

    first use

--- operating_cost_3.m
+++ operating_cost_3.m
@@ -68,7 +68,6 @@ delta(2,1:OPTIONS.N_t ) = 1;
 % 7 (Fault w PPA w ESMC)
 
 %% fault optimal cost finding
-% test diff function
 max_searchbit(1:2) = [4 7]; 
 for index_search = 1:1
     for operation_mode =  0:3
@@ -390,3 +389,4 @@ end
 
 end
          
+% test --
